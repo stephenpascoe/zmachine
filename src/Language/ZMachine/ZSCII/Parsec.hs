279a642f5c9ae4a7547427f5745015a5adc55431
Stephen Pascoe <stephen.pascoe@lirico.co.uk>
2019-04-11 10:38:12 +0100

    Handle trailing shift chars

--- src/Language/ZMachine/ZSCII/Parsec.hs
+++ src/Language/ZMachine/ZSCII/Parsec.hs
@@ -9,6 +9,7 @@ import Data.Word
 import Data.Vector ((!?))
 import Text.Parsec.Prim
 import Text.Parsec.Combinator
+import Data.Maybe
 
 import Language.ZMachine.Types
 import Language.ZMachine.ZSCII.ZChars
@@ -36,6 +37,7 @@ decodeZString :: Version                 -- ^ ZMachine version
               -> ZsciiString             -- ^ Resulting ZsciiString
 decodeZString version aTable zstr = zcharsToZscii version aTable (zstrToZchars zstr)
 
+
 zcharsToZscii :: Version -> Maybe AbbreviationTable -> [ZChar] -> ZsciiString
 zcharsToZscii version aTable zchars =
   let alphabetTable = getAlphabetTable version
@@ -45,7 +47,7 @@ zcharsToZscii version aTable zchars =
       pureChar = pure . fromIntegral . fromEnum
 
       parseZstring :: ZsciiParsec B.ByteString
-      parseZstring = B.pack <$> many element
+      parseZstring = B.pack . catMaybes <$> many element
 
       element = normal <|> special
 
@@ -58,6 +60,9 @@ zcharsToZscii version aTable zchars =
 
       specialChar = satisfy (\z -> z <= 6)
       normalChar = satisfy (\z -> z > 6 && z < 32)
+
+      -- Many combinators return Maybe a so we can change state without emmiting chars and
+      -- allowing us to handle padding at the end of a string.
 
       -- Alphabet shifting
       shiftUp = do a <- getState
@@ -73,12 +78,12 @@ zcharsToZscii version aTable zchars =
                                   Alpha2 -> Alpha1
 
       shiftUpOnce = do shiftUp
-                       e <- element
+                       e <- element <|> pure Nothing
                        shiftDown
                        return e
 
       shiftDownOnce = do shiftDown
-                         e <- element
+                         e <- element <|> pure Nothing
                          shiftUp
                          return e
 
@@ -87,32 +92,32 @@ zcharsToZscii version aTable zchars =
 
       specialV12 = do z <- specialChar
                       case z of
-                        0 -> pureChar ' '
-                        1 -> if version == 1 then pureChar '\n'
-                             else abbrev 1
+                        0 -> Just <$> pureChar ' '
+                        1 -> if version == 1 then Just <$> pureChar '\n'
+                             else Just <$> abbrev 1
                         2 -> shiftUpOnce
                         3 -> shiftDownOnce
                         -- TODO : 4/5 can occur at end of input, therefore element is optional
-                        4 -> shiftDown *> element
-                        5 -> shiftUp *> element
-                        6 -> pureChar '@' -- error "char 6" -- handle char 6 in A2
+                        4 -> shiftDown *> pure Nothing
+                        5 -> shiftUp *> pure Nothing
+                        6 -> Just <$> pureChar '@' -- error "char 6" -- handle char 6 in A2
                         _ -> error "Not a special character"
 
       specialV3 = do z <- specialChar
                      case z of
-                       0 -> pureChar ' '
-                       1 -> abbrev 1
-                       2 -> abbrev 2
-                       3 -> abbrev 3
+                       0 -> Just <$> pureChar ' '
+                       1 -> Just <$> abbrev 1
+                       2 -> Just <$> abbrev 2
+                       3 -> Just <$> abbrev 3
                        4 -> shiftUpOnce
                        5 -> shiftDownOnce
-                       6 -> pureChar '@' -- error "char 6" -- handle char 6 in A2
+                       6 -> Just <$> pureChar '@' -- error "char 6" -- handle char 6 in A2
                        _ -> error "Not a special character"
 
       -- Normal character parser
       normal = do z <- normalChar
                   alphabet <- getState
-                  return $ B.index (getAlphabetTable version alphabet) (fromIntegral (z - 6))
+                  return $ Just (B.index (getAlphabetTable version alphabet) (fromIntegral (z - 6)))
 
       -- Abbreviation parser
       -- TODO : Need to return ByteString
